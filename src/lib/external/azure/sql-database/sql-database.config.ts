495febb549d12a35a96d56513287cc8143f6cdbb
Daniel Salazar <daniel.esalazars@gmail.com>
2025-07-15 16:41:03 -0500

    Add files via upload

--- src/lib/external/azure/sql-database/sql-database.config.ts
+++ src/lib/external/azure/sql-database/sql-database.config.ts
@@ -1,15 +1,23 @@
 import { config } from 'mssql';
 
 export const sqlDatabaseConfig: config = {
-  user: process.env.DB_USER || 'sa',
-  password: process.env.DB_PASSWORD || 'YourStrong!Passw0rd',
+  // user: process.env.DB_USER || 'sa',
+  // password: process.env.DB_PASSWORD || 'YourStrong!Passw0rd',
   database: process.env.DB_NAME || 'applicationDB',
   server: process.env.DB_HOST || 'localhost', //name_container
-  port: parseInt(process.env.DB_PORT || '1433', 10),
+  // port: parseInt(process.env.DB_PORT || '1433', 10),
   options: {
-    encrypt: false, // Requerido para Azure
+    encrypt: true, // true = Azure , false Contenedor Local
     enableArithAbort: true,
     trustServerCertificate: true,
+  },
+  authentication:{
+    type: 'azure-active-directory-service-principal-secret',
+    options:{
+      clientId: process.env.AZURE_CLIENT_ID,
+      clientSecret: process.env.AZURE_CLIENT_SECRET,
+      tenantId: process.env.AZURE_TENANT_ID,
+    }
   },
   pool: {
     max: 10, // Número máximo de conexiones en el pool
